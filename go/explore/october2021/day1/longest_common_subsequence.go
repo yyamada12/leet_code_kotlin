137641d255e1922ba83c7fa4e2519c676826706a
yyamada12 <12yacropolisy@gmail.com>
2021-10-01 21:44:07 +0900

    Longest Common Subsequence

---
+++ go/explore/october2021/day1/longest_common_subsequence.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(longestCommonSubsequence("abcde", "ace"))
+}
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	l1 := len(text1)
+	l2 := len(text2)
+	dp := make([][]int, l1+1)
+	for i := 0; i < l1+1; i++ {
+		dp[i] = make([]int, l2+1)
+	}
+	for i := 1; i <= l1; i++ {
+		for j := 1; j <= l2; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[l1][l2]
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
